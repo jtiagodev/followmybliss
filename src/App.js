fc6303e983967f5945ae3f14eeab160fa032f84f
João Tiago <jtiagodev@gmail.com>
2020-10-25 03:29:58 +0000

    feat(router): basic router setup with connectivity listener

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "../src/containers/presentation/Home";
+import NoConnectivityScreen from "../src/containers/presentation/NoConnectivityScreen";
 import QuestionsListScreen from "../src/containers/presentation/QuestionsListScreen";
 import NotFoundScreen from "../src/containers/presentation/NotFoundScreen";
 
 const App = (props) => {
+  const [connectionStatus, setConnectionStatus] = useState("ONLINE");
+
+  useEffect(() => {
+    const setOffline = (event) => {
+      setConnectionStatus("OFFLINE");
+    };
+    const setOnline = (event) => {
+      setConnectionStatus("ONLINE");
+    };
+    window.addEventListener("offline", setOffline);
+    window.addEventListener("online", setOnline);
+
+    return function cleanup() {
+      window.removeEventListener("online", setOnline);
+      window.removeEventListener("offline", setOffline);
+    };
+  }, []);
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/questions">
-          <QuestionsListScreen />
-        </Route>
-        <Route path="*">
-          <NotFoundScreen />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      {connectionStatus === "OFFLINE" && <NoConnectivityScreen />}
+      {connectionStatus === "ONLINE" && (
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <QuestionsListScreen />
+            </Route>
+            <Route path="/questions">
+              <QuestionsListScreen />
+            </Route>
+            <Route path="*">
+              <NotFoundScreen />
+            </Route>
+          </Switch>
+        </Router>
+      )}
+    </>
   );
 };
 
